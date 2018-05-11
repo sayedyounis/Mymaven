6c3e565a193244fa6a0f1309fdebb3e7ea35ff8f
sayedyounispasha <sdpasha413@gmail.com>
2018-05-11 12:53:28 +0530

    maven project

---
+++ Mymavenproject/src/main/java/www/innoitus/com/App.java
@@ -0,0 +1,13 @@
+package www.innoitus.com;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
